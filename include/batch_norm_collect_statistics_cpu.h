5ac7d78f41f5126a4d7be842f21cd915d10fd5a4
Jiahang Xie <1351533120@sjtu.edu.cn>
2024-04-23 19:33:49 +0800

    Minor fix

--- include/batch_norm_collect_statistics_cpu.h
+++ include/batch_norm_collect_statistics_cpu.h
@@ -1,6 +1,11 @@
+#ifndef __BATCH_NORM_COLLECT_STATISTICS_CPU_H__
+#define __BATCH_NORM_COLLECT_STATISTICS_CPU_H__
+
 #include <math.h>
 
 void batch_norm_collect_statistics_cpu(const float* input, int height,
                                        int width, int depth, float epsilon,
                                        float* save_mean,
                                        float* save_transformed_var);
+
+#endif  // __BATCH_NORM_COLLECT_STATISTICS_CPU_H__
